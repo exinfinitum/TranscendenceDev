5a0e79635cd61e7612230a175b869316de82d4ba
Bryon Leung <teslaslegacy@gmail.com>
2020-10-20 00:38:17 -0400

    Fix half of the glowmap addressing problem during glowmap generation...

--- Transcendence/Game/shaders/glowmap_fragment_shader.glsl
+++ Transcendence/Game/shaders/glowmap_fragment_shader.glsl
@@ -3,6 +3,8 @@ out vec4 FragColor;
   
 layout (location = 0) in vec2 TexCoord;
 
+uniform vec2 aTexStartPoint;
+uniform vec2 aTexQuadSizes;
 uniform vec2 gridSquareSize;
 uniform sampler2D ourTexture;
 uniform int kernelSize;
@@ -44,8 +46,9 @@ float getSumGauss(int kernel_size)
 }
 
 ivec2 getPixelGridSquareUnpadded(vec2 coords) {
-    int x_coord = int(coords[0] / gridSquareSize[0]);
-	int y_coord = int(coords[1] / gridSquareSize[1]);
+    vec2 adjusted_coords = coords - aTexStartPoint;
+    int x_coord = int(adjusted_coords[0] / gridSquareSize[0]);
+	int y_coord = int(adjusted_coords[1] / gridSquareSize[1]);
 	return ivec2(x_coord, y_coord);
 }
 
@@ -58,7 +61,7 @@ ivec2 getPixelGridSquarePadded(vec2 coords, vec2 pad_fraction_of_output_image) {
 vec4 getGlowBoundaries_variable(float epsilon, vec2 texture_uv, sampler2D obj_texture, vec2 texture_size_input, int glow_size, float sum_gauss, bool using_x_axis, bool additive, vec2 pad_fraction_of_output_image, vec2 texture_size_output)
 {
 	float percent_std_dev = 0.66;
-    vec2 onePixel = vec2(1.0, 1.0) / texture_size_output;
+    vec2 onePixel = vec2(1.0, 1.0) / texture_size_input;
     vec2 quadSize_float = gridSquareSize;//
 	ivec2 quad_index = getPixelGridSquareUnpadded(texture_uv);
     int center = int(glow_size / 2);
@@ -80,18 +83,25 @@ vec4 getGlowBoundaries_variable(float epsilon, vec2 texture_uv, sampler2D obj_te
 		// output; this is the pixel coordinates we need to sample from in the input. Note that if we apply the subtraction
 		// and apply zero minimum bound, then padded pixels in output image will map to row or column zero in the input image
 		// or map to out-of-quad locations, so they will effectively be zero.
-        vec2 texCoords0 =  vec2(texture_uv[0], texture_uv[1]) + offset0;
+        vec2 texCoords0 =  vec2(texture_uv[0], texture_uv[1]);
+		texCoords0 = texCoords0 - aTexStartPoint;
+
 		texCoords0 = texCoords0 * texture_size_output;
 		texCoords0 = texCoords0 - (((2 * quad_index) + vec2(1.0, 1.0)) * pad_pixels_per_grid_square);
 		texCoords0 = texCoords0 / texture_size_input;
+		//texCoords0 = texCoords0 * (aTexQuadSizes + ((2 * pad_pixels_per_grid_square * num_grid_squares) / texture_size_input)) / aTexQuadSizes;
+		texCoords0 = texCoords0 + aTexStartPoint;
+		texCoords0 = texCoords0 + offset0;
         float texAlpha0 = float(texture(obj_texture, texCoords0)[3]);
-        bool texInBounds0 = getPixelGridSquareUnpadded(texCoords0) == quad_index && texCoords0[0] > 0.0f && texCoords0[0] < 1.0f && texCoords0[1] > 0.0f && texCoords0[1] < 1.0f;
+		vec2 texEndPoint = aTexStartPoint + aTexQuadSizes;
+        bool texInBounds0 = getPixelGridSquareUnpadded(texCoords0) == quad_index && texCoords0[0] > aTexStartPoint[0] && texCoords0[0] < texEndPoint[0] && texCoords0[1] > aTexStartPoint[1] && texCoords0[1] < texEndPoint[1];
         float sampleR = min(1.0, float(texture(obj_texture, texCoords0)[0])) * (gauss_val / sum_gauss) * float(texInBounds0);
 		float sampleG = min(1.0, float(texture(obj_texture, texCoords0)[1])) * (gauss_val / sum_gauss) * float(texInBounds0);
 		float sampleB = min(1.0, float(texture(obj_texture, texCoords0)[2])) * (gauss_val / sum_gauss) * float(texInBounds0);
 		float sampleA = min(1.0, float(texture(obj_texture, texCoords0)[3] > epsilon)) * (gauss_val / sum_gauss) * float(texInBounds0);
 		sampleA = (float(!additive) * sampleA) + (float(additive) * (sampleA * texture(obj_texture, texCoords0)[3]));
 		glowBoundaries = (vec4(sampleR, sampleG, sampleB, sampleA) + glowBoundaries);
+		//glowBoundaries = (vec4(texCoords0[0], texCoords0[1], 0.0, 1.0) + glowBoundaries);
 	}
 	return vec4(glowBoundaries[0], glowBoundaries[1], glowBoundaries[2], min(1.0, 2.0 * glowBoundaries[3]));
 	
@@ -101,7 +111,7 @@ void main()
 {
 	float epsilon = 0.0001;
 	vec2 texture_size_input = textureSize(ourTexture, 0);
-	vec2 texture_size_output = textureSize(ourTexture, 0) + (2 * pad_pixels_per_grid_square * num_grid_squares);
+	vec2 texture_size_output = (textureSize(ourTexture, 0)) + ((1.0 / gridSquareSize) * (2 * pad_pixels_per_grid_square));
 	vec2 pad_fraction_of_output_image = pad_pixels_per_grid_square / texture_size_output;
 	float sumGauss = getSumGauss(kernelSize);
 	vec4 glowValue = getGlowBoundaries_variable(epsilon, TexCoord, ourTexture, texture_size_input, kernelSize, sumGauss, (float(use_x_axis) > epsilon), bool(second_pass), pad_fraction_of_output_image, texture_size_output);
