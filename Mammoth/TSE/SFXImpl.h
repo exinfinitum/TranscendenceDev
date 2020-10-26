f2bd2f067ffa1c71e47d57487d2261ff4a76050d
Bryon Leung <teslaslegacy@gmail.com>
2020-10-25 23:35:20 -0400

    Add <UseSourceObj> attribute to SFXGlow

--- Mammoth/TSE/SFXImpl.h
+++ Mammoth/TSE/SFXImpl.h
@@ -383,6 +383,7 @@ class CGlowEffectCreator : public CEffectCreator
 
 		CEffectParamDesc m_Lifetime;		//	lifetime: Lifetime in ticks (optional)
 		CEffectParamDesc m_Animate;			//	animate: Animation style
+		bool m_UseSourceObj;				//  useSourceObj: whether to use source obj for generating glow image
 
 		IEffectPainter *m_pSingleton = NULL;
 	};
