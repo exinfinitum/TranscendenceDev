07cff3b2de2602e03a9ae0de1952683c3cd5a080
Bryon Leung <teslaslegacy@gmail.com>
2020-12-19 02:03:03 -0500

    Finish up remaining tasks for 'targetCriteria

--- Mammoth/TSE/CShip.cpp
+++ Mammoth/TSE/CShip.cpp
@@ -8032,7 +8032,7 @@ void CShip::SetWeaponTriggered (DeviceNames iDev, bool bTriggered)
 		//	is linked to the primary device, then activate it.
 
 		if (&Device == pPrimaryDevice
-				|| (Device.IsLinkedFire(iCat, pPrimaryDevice)))
+				|| (Device.IsLinkedFire(iCat, pPrimaryDevice) && !Device.IsAutomatedWeapon()))
 			Device.SetTriggered(bTriggered);
 		}
 	}
@@ -8057,7 +8057,7 @@ void CShip::SetWeaponTriggered (CInstalledDevice *pWeapon, bool bTriggered)
 		//	is linked to the primary device, then activate it.
 		CInstalledDevice* pDevice = &Device;
 		if (pDevice == pWeapon
-				|| (Device.IsLinkedFire(iCat, pWeapon)))
+				|| (Device.IsLinkedFire(iCat, pWeapon) && !Device.IsAutomatedWeapon()))
 			Device.SetTriggered(bTriggered);
 		}
 	}
