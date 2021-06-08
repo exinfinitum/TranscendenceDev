93ab45329ffd70805826ede459bcfddc8448c5b3
Bryon Leung <teslaslegacy@gmail.com>
2021-06-07 23:42:47 -0400

    Use statically linked GLEW

--- Alchemy/Include/OpenGLIncludes.h
+++ Alchemy/Include/OpenGLIncludes.h
@@ -1,5 +1,6 @@
 #pragma once
-
+// To compile statically linked TOGL, "glew32.lib" must NOT be present in vcpkg\installed\x86-windows\lib - use "glew32s.lib" instead of "glew32.lib".
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GL/wglew.h>
 //#pragma comment(lib, "glew32.lib")
